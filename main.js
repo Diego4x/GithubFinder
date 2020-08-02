0c36796b459c9fd343e93dfef5678cf4254f78a3
Diego4x <60366577+Diego4x@users.noreply.github.com>
2020-08-02 20:21:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const texto = document.getElementById('txt')
 const button= document.getElementById('btn')
+const err = document.getElementById('error')
 async function getUser() {
   try {
     const text = texto.value;
     const res = await fetch(`https://api.github.com/users/${text}`)
     const resp = await res.json()
     if (text.length == 0) {
-      alert('error falta dados')
+      err.innerHTML = `
+       <img src="https://png2.kisspng.com/sh/4c52fbd2fdf3c698786800a3422a10be/L0KzQYm3VsI5N6pAhJH0aYP2gLBuTfdqfJl6ep98b3Pscb20jfVlcZIye9H2cIX3dcO0ifNwdqQyhNHwbz3kfrX5jBllNWZnS6YDNEmzcbO5UPYyNmY5UKs9MEa3QYa6UME3QGU7TKk5MT7zfri=/kisspng-github-social-media-computer-icons-logo-android-5b348490ab20f1.548940641530168464701.png" alt="error" />
+       <h1>Para visualizar um perfil digite um usuário por favor</h1>
+      `
     } else {
       console.log(resp);
       mostrar(resp)
@@ -42,5 +46,3 @@ function mostrar(resposta){
   `
 }
 button.onclick = getUser
-
-
